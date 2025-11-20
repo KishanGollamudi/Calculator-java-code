a2c614f99340b27fce1ae1f2aab7cae9428446d1
KishanGollamudi <gnmskishan@gmail.com>
2025-11-20 11:29:35 +0530

    Create HomeController.java

---
+++ src/main/java/com/houarizegai/calculator/controller/HomeController.java
@@ -0,0 +1,13 @@
+package com.houarizegai.calculator.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HomeController {
+
+    @GetMapping("/")
+    public String home() {
+        return "Calculator Backend is Running Successfully!";
+    }
+}
